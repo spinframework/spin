4162373616f5ce1ca240732a5a0440188afc2189
itowlson <ivan.towlson@fermyon.com>
2026-03-17 15:13:15 +1300

    Async variables
    Signed-off-by: itowlson <ivan.towlson@fermyon.com>

--- crates/factor-variables/src/host.rs
+++ crates/factor-variables/src/host.rs
@@ -1,13 +1,40 @@
+use spin_core::wasmtime::component::Accessor;
 use spin_factors::anyhow;
 use spin_telemetry::traces::{self, Blame};
-use spin_world::{v1, v2::variables, wasi::config as wasi_config};
+use spin_world::{
+    spin::variables::variables as v3, v1, v2::variables as v2, wasi::config as wasi_config,
+};
 use tracing::instrument;
 
-use crate::InstanceState;
+use crate::{InstanceState, VariablesFactorData};
 
-impl variables::Host for InstanceState {
+impl v3::HostWithStore for VariablesFactorData {
+    #[instrument(name = "spin_variables.get", skip(accessor), fields(otel.kind = "client"))]
+    async fn get<T: Send>(accessor: &Accessor<T, Self>, key: String) -> Result<String, v3::Error> {
+        let (resolver, component_id) = accessor.with(|mut access| {
+            let host = access.get();
+            host.otel.reparent_tracing_span();
+            (host.expression_resolver.clone(), host.component_id.clone())
+        });
+
+        let key = spin_expressions::Key::new(&key).map_err(expressions_to_variables_err_v3)?;
+
+        resolver
+            .resolve(&component_id, key)
+            .await
+            .map_err(expressions_to_variables_err_v3)
+    }
+}
+
+impl v3::Host for InstanceState {
+    fn convert_error(&mut self, err: v3::Error) -> anyhow::Result<v3::Error> {
+        Ok(err)
+    }
+}
+
+impl v2::Host for InstanceState {
     #[instrument(name = "spin_variables.get", skip(self), fields(otel.kind = "client"))]
-    async fn get(&mut self, key: String) -> Result<String, variables::Error> {
+    async fn get(&mut self, key: String) -> Result<String, v2::Error> {
         self.otel.reparent_tracing_span();
         let key = spin_expressions::Key::new(&key).map_err(expressions_to_variables_err)?;
         self.expression_resolver
@@ -16,7 +43,7 @@ impl variables::Host for InstanceState {
             .map_err(expressions_to_variables_err)
     }
 
-    fn convert_error(&mut self, error: variables::Error) -> anyhow::Result<variables::Error> {
+    fn convert_error(&mut self, error: v2::Error) -> anyhow::Result<v2::Error> {
         Ok(error)
     }
 }
@@ -24,11 +51,11 @@ impl variables::Host for InstanceState {
 impl v1::config::Host for InstanceState {
     #[instrument(name = "spin_config.get", skip(self), fields(otel.kind = "client"))]
     async fn get_config(&mut self, key: String) -> Result<String, v1::config::Error> {
-        <Self as variables::Host>::get(self, key)
+        <Self as v2::Host>::get(self, key)
             .await
             .map_err(|err| match err {
-                variables::Error::InvalidName(msg) => v1::config::Error::InvalidKey(msg),
-                variables::Error::Undefined(msg) => v1::config::Error::Provider(msg),
+                v2::Error::InvalidName(msg) => v1::config::Error::InvalidKey(msg),
+                v2::Error::Undefined(msg) => v1::config::Error::Provider(msg),
                 other => v1::config::Error::Other(format!("{other}")),
             })
     }
@@ -41,12 +68,12 @@ impl v1::config::Host for InstanceState {
 impl wasi_config::store::Host for InstanceState {
     #[instrument(name = "wasi_config.get", skip(self), fields(otel.kind = "client"))]
     async fn get(&mut self, key: String) -> Result<Option<String>, wasi_config::store::Error> {
-        match <Self as variables::Host>::get(self, key).await {
+        match <Self as v2::Host>::get(self, key).await {
             Ok(value) => Ok(Some(value)),
-            Err(variables::Error::Undefined(_)) => Ok(None),
-            Err(variables::Error::InvalidName(_)) => Ok(None), // this is the guidance from https://github.com/WebAssembly/wasi-runtime-config/pull/19)
-            Err(variables::Error::Provider(msg)) => Err(wasi_config::store::Error::Upstream(msg)),
-            Err(variables::Error::Other(msg)) => Err(wasi_config::store::Error::Io(msg)),
+            Err(v2::Error::Undefined(_)) => Ok(None),
+            Err(v2::Error::InvalidName(_)) => Ok(None), // this is the guidance from https://github.com/WebAssembly/wasi-runtime-config/pull/19)
+            Err(v2::Error::Provider(msg)) => Err(wasi_config::store::Error::Upstream(msg)),
+            Err(v2::Error::Other(msg)) => Err(wasi_config::store::Error::Io(msg)),
         }
     }
 
@@ -58,10 +85,10 @@ impl wasi_config::store::Host for InstanceState {
             .await;
         all.map_err(|e| {
             match expressions_to_variables_err(e) {
-                variables::Error::Undefined(msg) => wasi_config::store::Error::Io(msg), // this shouldn't happen but just in case
-                variables::Error::InvalidName(msg) => wasi_config::store::Error::Io(msg), // this shouldn't happen but just in case
-                variables::Error::Provider(msg) => wasi_config::store::Error::Upstream(msg),
-                variables::Error::Other(msg) => wasi_config::store::Error::Io(msg),
+                v2::Error::Undefined(msg) => wasi_config::store::Error::Io(msg), // this shouldn't happen but just in case
+                v2::Error::InvalidName(msg) => wasi_config::store::Error::Io(msg), // this shouldn't happen but just in case
+                v2::Error::Provider(msg) => wasi_config::store::Error::Upstream(msg),
+                v2::Error::Other(msg) => wasi_config::store::Error::Io(msg),
             }
         })
     }
@@ -74,8 +101,8 @@ impl wasi_config::store::Host for InstanceState {
     }
 }
 
-/// Convert a `spin_expressions::Error` to a `variables::Error`, setting the current span's status and fault attribute.
-fn expressions_to_variables_err(err: spin_expressions::Error) -> variables::Error {
+/// Convert a `spin_expressions::Error` to a `v2::Error`, setting the current span's status and fault attribute.
+fn expressions_to_variables_err(err: spin_expressions::Error) -> v2::Error {
     use spin_expressions::Error;
     let blame = match err {
         Error::InvalidName(_) | Error::InvalidTemplate(_) | Error::Undefined(_) => Blame::Guest,
@@ -83,9 +110,25 @@ fn expressions_to_variables_err(err: spin_expressions::Error) -> variables::Erro
     };
     traces::mark_as_error(&err, Some(blame));
     match err {
-        Error::InvalidName(msg) => variables::Error::InvalidName(msg),
-        Error::Undefined(msg) => variables::Error::Undefined(msg),
-        Error::InvalidTemplate(_) => variables::Error::Other(format!("{err}")),
-        Error::Provider(err) => variables::Error::Provider(err.to_string()),
+        Error::InvalidName(msg) => v2::Error::InvalidName(msg),
+        Error::Undefined(msg) => v2::Error::Undefined(msg),
+        Error::InvalidTemplate(_) => v2::Error::Other(format!("{err}")),
+        Error::Provider(err) => v2::Error::Provider(err.to_string()),
+    }
+}
+
+/// Convert a `spin_expressions::Error` to a `v3::Error`, setting the current span's status and fault attribute.
+fn expressions_to_variables_err_v3(err: spin_expressions::Error) -> v3::Error {
+    use spin_expressions::Error;
+    let blame = match err {
+        Error::InvalidName(_) | Error::InvalidTemplate(_) | Error::Undefined(_) => Blame::Guest,
+        Error::Provider(_) => Blame::Host,
+    };
+    traces::mark_as_error(&err, Some(blame));
+    match err {
+        Error::InvalidName(msg) => v3::Error::InvalidName(msg),
+        Error::Undefined(msg) => v3::Error::Undefined(msg),
+        Error::InvalidTemplate(_) => v3::Error::Other(format!("{err}")),
+        Error::Provider(err) => v3::Error::Provider(err.to_string()),
     }
 }
