6d4f40ae9c365079be72f24960fd6c1b6e855867
itowlson <ivan.towlson@fermyon.com>
2026-03-27 14:29:21 +1300

    Prepare templates for SDK changes
    Signed-off-by: itowlson <ivan.towlson@fermyon.com>

--- templates/http-rust/content/src/lib.rs
+++ templates/http-rust/content/src/lib.rs
@@ -1,13 +1,12 @@
 use spin_sdk::http::{IntoResponse, Request, Response};
-use spin_sdk::http_component;
+use spin_sdk::http_service;
 
 /// A simple Spin HTTP component.
-#[http_component]
-fn handle_{{project-name | snake_case}}(req: Request) -> anyhow::Result<impl IntoResponse> {
-    println!("Handling request to {:?}", req.header("spin-full-url"));
+#[http_service]
+async fn handle_{{project-name | snake_case}}(req: Request) -> anyhow::Result<impl IntoResponse> {
+    println!("Handling request to {:?}", req.headers().get("spin-full-url"));
     Ok(Response::builder()
         .status(200)
         .header("content-type", "text/plain")
-        .body("Hello World!")
-        .build())
+        .body("Hello World!".to_string())?)
 }
