6d9e8c79c94d0153d7c9377e4d4bb9b816fa1a99
spinframeworkbot <202838904+spinframeworkbot@users.noreply.github.com>
2026-04-20 18:41:31 -0600

    chore(rust-sdk-release): bump Spin Rust SDK to v6.0.0 (#3472)
    * chore(rust-sdk-release): bump Spin Rust SDK to v6.0.0
    
    Signed-off-by: spinframeworkbot <202838904+spinframeworkbot@users.noreply.github.com>
    Signed-off-by: Brian Hardock <brian.hardock@fermyon.com>
    
    * Fix examples
    
    Signed-off-by: Brian Hardock <brian.hardock@fermyon.com>
    
    * Update rust templates
    
    Signed-off-by: Brian Hardock <brian.hardock@fermyon.com>
    
    ---------
    
    Signed-off-by: spinframeworkbot <202838904+spinframeworkbot@users.noreply.github.com>
    Signed-off-by: Brian Hardock <brian.hardock@fermyon.com>
    Co-authored-by: Brian Hardock <brian.hardock@fermyon.com>

--- examples/spin-wagi-http/http-rust/src/lib.rs
+++ examples/spin-wagi-http/http-rust/src/lib.rs
@@ -1,12 +1,13 @@
 use anyhow::Result;
-use spin_sdk::http_component;
+use spin_sdk::http::{IntoResponse, Request};
+use spin_sdk::http_service;
 
 /// A simple Spin HTTP component.
-#[http_component]
-fn goodbye_world(req: http::Request<()>) -> Result<http::Response<&'static str>> {
+#[http_service]
+async fn goodbye_world(req: Request) -> Result<impl IntoResponse> {
     println!("{:?}", req.headers());
     Ok(http::Response::builder()
         .status(200)
         .header("foo", "bar")
-        .body("Goodbye, World!\n")?)
+        .body("Goodbye, World!\n".to_string())?)
 }
