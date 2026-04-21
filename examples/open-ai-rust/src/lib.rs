f62c68d88dbfa2ecf027cbab7365d3e254b292e5
spinframeworkbot <202838904+spinframeworkbot@users.noreply.github.com>
2026-04-20 18:41:31 -0600

    chore(rust-sdk-release): bump Spin Rust SDK to v6.0.0 (#3472)
    * chore(rust-sdk-release): bump Spin Rust SDK to v6.0.0
    
    Signed-off-by: spinframeworkbot <202838904+spinframeworkbot@users.noreply.github.com>
    Signed-off-by: Brian Hardock <brian.hardock@fermyon.com>
    
    * Fix examples
    
    Signed-off-by: Brian Hardock <brian.hardock@fermyon.com>
    
    * Update rust templates
    
    Signed-off-by: Brian Hardock <brian.hardock@fermyon.com>
    
    ---------
    
    Signed-off-by: spinframeworkbot <202838904+spinframeworkbot@users.noreply.github.com>
    Signed-off-by: Brian Hardock <brian.hardock@fermyon.com>
    Co-authored-by: Brian Hardock <brian.hardock@fermyon.com>
    (cherry picked from commit 6d9e8c79c94d0153d7c9377e4d4bb9b816fa1a99)

--- examples/open-ai-rust/src/lib.rs
+++ examples/open-ai-rust/src/lib.rs
@@ -1,15 +1,18 @@
 use spin_sdk::http::{IntoResponse, Request, Response};
-use spin_sdk::http_component;
+use spin_sdk::http_service;
 
 /// A simple Spin HTTP component.
-#[http_component]
-fn handle_open_ai_rust(req: Request) -> anyhow::Result<impl IntoResponse> {
+#[http_service]
+async fn handle_open_ai_rust(req: Request) -> anyhow::Result<impl IntoResponse> {
     let llm_chat = spin_sdk::llm::infer(
         spin_sdk::llm::InferencingModel::Other("gpt-oss:20b"),
         "tell me about Epe in Lagos, Nigeria",
     )?;
 
-    println!("Handling request to {:?}", req.header("spin-full-url"));
+    println!(
+        "Handling request to {:?}",
+        req.headers().get("spin-full-url")
+    );
 
     Ok(Response::builder()
         .status(200)
@@ -17,6 +20,5 @@ fn handle_open_ai_rust(req: Request) -> anyhow::Result<impl IntoResponse> {
         .body(format!(
             "Here's your response: {}\n Total tokens used: {}",
             llm_chat.text, llm_chat.usage.prompt_token_count
-        ))
-        .build())
+        )))
 }
