ee327f1983e900a1b06319bdcc0126503ea8bcb6
Ryan Levick <rlevick@akamai.com>
2026-04-24 16:05:41 +0200

    Set blame on span for postgres and mysql
    Signed-off-by: Ryan Levick <rlevick@akamai.com>
    (cherry picked from commit f45501322f90845ca28c63fdda4b781f6cdac8fa)

--- crates/factor-outbound-pg/src/host.rs
+++ crates/factor-outbound-pg/src/host.rs
@@ -2,6 +2,7 @@
 
 use anyhow::Result;
 use spin_core::wasmtime::component::{Accessor, FutureReader, Resource, StreamReader};
+use spin_telemetry::traces::{self, Blame};
 use spin_world::MAX_HOST_BUFFERED_BYTES;
 use spin_world::spin::postgres3_0_0::postgres::{self as v3};
 use spin_world::spin::postgres4_2_0::postgres::{self as v4};
@@ -23,14 +24,26 @@ impl<CF: ClientFactory> InstanceState<CF> {
         address: &str,
         root_ca: Option<HashableCertificate>,
     ) -> Result<Resource<Conn>, v4::Error> {
+        let client = self
+            .client_factory
+            .get_client(address, root_ca)
+            .await
+            .map_err(|e| {
+                // The guest supplies the address and credentials; connection
+                // failures (wrong password, TLS error, unreachable host, etc.)
+                // are the guest's problem.
+                let err = v4::Error::ConnectionFailed(format!("{e:?}"));
+                traces::mark_as_error(&err, Some(Blame::Guest));
+                err
+            })?;
         self.connections
-            .push(
-                self.client_factory
-                    .get_client(address, root_ca)
-                    .await
-                    .map_err(|e| v4::Error::ConnectionFailed(format!("{e:?}")))?,
-            )
-            .map_err(|_| v4::Error::ConnectionFailed("too many connections".into()))
+            .push(client)
+            .map_err(|_| {
+                // The guest exceeded the host-imposed connection limit.
+                let err = v4::Error::ConnectionFailed("too many connections".into());
+                traces::mark_as_error(&err, Some(Blame::Guest));
+                err
+            })
             .map(Resource::new_own)
     }
 
@@ -38,9 +51,13 @@ impl<CF: ClientFactory> InstanceState<CF> {
         &self,
         connection: Resource<Conn>,
     ) -> Result<&CF::Client, v4::Error> {
-        self.connections
-            .get(connection.rep())
-            .ok_or_else(|| v4::Error::ConnectionFailed("no connection found".into()))
+        self.connections.get(connection.rep()).ok_or_else(|| {
+            // The connection table is managed entirely by the host, so a
+            // missing handle indicates a host-side bug, not a guest mistake.
+            let err = v4::Error::ConnectionFailed("no connection found".into());
+            traces::mark_as_error(&err, Some(Blame::Host));
+            err
+        })
     }
 
     fn allowed_host_checker(&self) -> AllowedHostChecker {
@@ -70,9 +87,14 @@ impl<CF: ClientFactory> v3::HostConnection for InstanceState<CF> {
     async fn open(&mut self, address: String) -> Result<Resource<v3::Connection>, v3::Error> {
         spin_factor_outbound_networking::record_address_fields(&address);
 
-        self.ensure_address_allowed(&address).await?;
+        self.ensure_address_allowed(&address)
+            .await
+            .map_err(v3::Error::from)
+            .map_err(track_address_check_error_v3)?;
 
-        Ok(self.open_connection(&address, None).await?)
+        self.open_connection(&address, None)
+            .await
+            .map_err(v3::Error::from)
     }
 
     #[instrument(name = "spin_outbound_pg.execute", skip(self, connection, params), err(level = Level::INFO), fields(otel.kind = "client", db.system = "postgresql", otel.name = statement))]
@@ -82,11 +104,13 @@ impl<CF: ClientFactory> v3::HostConnection for InstanceState<CF> {
         statement: String,
         params: Vec<v3::ParameterValue>,
     ) -> Result<u64, v3::Error> {
-        Ok(self
-            .get_client(connection)
-            .await?
+        self.get_client(connection)
+            .await
+            .map_err(v3::Error::from)?
             .execute(statement, v3_params_to_v4(params))
-            .await?)
+            .await
+            .map_err(v3::Error::from)
+            .map_err(track_db_error_on_span_v3)
     }
 
     #[instrument(name = "spin_outbound_pg.query", skip(self, connection, params), err(level = Level::INFO), fields(otel.kind = "client", db.system = "postgresql", otel.name = statement))]
@@ -96,12 +120,15 @@ impl<CF: ClientFactory> v3::HostConnection for InstanceState<CF> {
         statement: String,
         params: Vec<v3::ParameterValue>,
     ) -> Result<v3::RowSet, v3::Error> {
-        Ok(self
+        let rowset = self
             .get_client(connection)
-            .await?
+            .await
+            .map_err(v3::Error::from)?
             .query(statement, v3_params_to_v4(params), MAX_HOST_BUFFERED_BYTES)
-            .await?
-            .into())
+            .await
+            .map_err(v3::Error::from)
+            .map_err(track_db_error_on_span_v3)?;
+        Ok(rowset.into())
     }
 
     async fn drop(&mut self, connection: Resource<v3::Connection>) -> anyhow::Result<()> {
@@ -134,12 +161,16 @@ impl<CF: ClientFactory> v4::HostConnectionBuilder for InstanceState<CF> {
         self_: Resource<v4::ConnectionBuilder>,
         certificate: String,
     ) -> Result<(), v4::Error> {
-        let root_ca = HashableCertificate::from_pem(&certificate)
-            .map_err(|e| v4::Error::Other(format!("invalid root certificate: {e}")))?;
-        let builder = self
-            .builders
-            .get_mut(self_.rep())
-            .ok_or_else(|| v4::Error::ConnectionFailed("no builder found".into()))?;
+        let root_ca = HashableCertificate::from_pem(&certificate).map_err(|e| {
+            let err = v4::Error::Other(format!("invalid root certificate: {e}"));
+            traces::mark_as_error(&err, Some(Blame::Guest));
+            err
+        })?;
+        let builder = self.builders.get_mut(self_.rep()).ok_or_else(|| {
+            let err = v4::Error::ConnectionFailed("no builder found".into());
+            traces::mark_as_error(&err, Some(Blame::Host));
+            err
+        })?;
         builder.root_ca = Some(root_ca);
         Ok(())
     }
@@ -163,7 +194,9 @@ impl<CF: ClientFactory> v4::HostConnection for InstanceState<CF> {
     async fn open(&mut self, address: String) -> Result<Resource<v4::Connection>, v4::Error> {
         spin_factor_outbound_networking::record_address_fields(&address);
 
-        self.ensure_address_allowed(&address).await?;
+        self.ensure_address_allowed(&address)
+            .await
+            .map_err(track_address_check_error_v4)?;
 
         self.open_connection(&address, None).await
     }
@@ -179,6 +212,7 @@ impl<CF: ClientFactory> v4::HostConnection for InstanceState<CF> {
             .await?
             .execute(statement, params)
             .await
+            .map_err(track_db_error_on_span_v4)
     }
 
     #[instrument(name = "spin_outbound_pg.query", skip(self, connection, params), err(level = Level::INFO), fields(otel.kind = "client", db.system = "postgresql", otel.name = statement))]
@@ -192,6 +226,7 @@ impl<CF: ClientFactory> v4::HostConnection for InstanceState<CF> {
             .await?
             .query(statement, params, MAX_HOST_BUFFERED_BYTES)
             .await
+            .map_err(track_db_error_on_span_v4)
     }
 
     async fn drop(&mut self, connection: Resource<v4::Connection>) -> anyhow::Result<()> {
@@ -210,7 +245,9 @@ impl<CF: ClientFactory> spin_world::spin::postgres4_2_0::postgres::HostConnectio
     ) -> Result<Resource<v4::Connection>, v4::Error> {
         spin_factor_outbound_networking::record_address_fields(&address);
 
-        Self::ensure_address_allowed_async(accessor, &address).await?;
+        Self::ensure_address_allowed_async(accessor, &address)
+            .await
+            .map_err(track_address_check_error_v4)?;
         Self::open_connection_async(accessor, &address, None).await
     }
 
@@ -226,7 +263,10 @@ impl<CF: ClientFactory> spin_world::spin::postgres4_2_0::postgres::HostConnectio
             host.connections.get(connection.rep()).unwrap().clone()
         });
 
-        client.execute(statement, params).await
+        client
+            .execute(statement, params)
+            .await
+            .map_err(track_db_error_on_span_v4)
     }
 
     #[allow(clippy::type_complexity)] // blame bindgen, clippy, blame bindgen
@@ -255,7 +295,8 @@ impl<CF: ClientFactory> spin_world::spin::postgres4_2_0::postgres::HostConnectio
             error,
         } = client
             .query_async(statement, params, MAX_HOST_BUFFERED_BYTES)
-            .await?;
+            .await
+            .map_err(track_db_error_on_span_v4)?;
 
         let row_producer = spin_wasi_async::stream::producer(rows);
 
@@ -265,7 +306,13 @@ impl<CF: ClientFactory> spin_world::spin::postgres4_2_0::postgres::HostConnectio
                 let efr = FutureReader::new(&mut access, error)?;
                 anyhow::Ok((sr, efr))
             })
-            .map_err(|e| v4::Error::Other(e.to_string()))?;
+            .map_err(|e| {
+                // Setting up the async stream/future channels is a host
+                // implementation detail; if it fails, that's a host bug.
+                let err = v4::Error::Other(e.to_string());
+                traces::mark_as_error(&err, Some(Blame::Host));
+                err
+            })?;
 
         Ok((columns, sr, efr))
     }
@@ -277,10 +324,11 @@ impl<CF: ClientFactory> InstanceState<CF> {
         &mut self,
         builder_rep: u32,
     ) -> Result<(String, Option<HashableCertificate>), v4::Error> {
-        let builder = self
-            .builders
-            .get_mut(builder_rep)
-            .ok_or_else(|| v4::Error::ConnectionFailed("no builder found".into()))?;
+        let builder = self.builders.get_mut(builder_rep).ok_or_else(|| {
+            let err = v4::Error::ConnectionFailed("no builder found".into());
+            traces::mark_as_error(&err, Some(Blame::Host));
+            err
+        })?;
 
         let address = builder.address.clone();
         let root_ca = builder.root_ca.clone();
@@ -325,16 +373,21 @@ impl<CF: ClientFactory> crate::PgFactorData<CF> {
             host.client_factory.clone()
         });
 
-        let client = cf
-            .get_client(address, root_ca)
-            .await
-            .map_err(|e| v4::Error::ConnectionFailed(format!("{e:?}")))?;
+        let client = cf.get_client(address, root_ca).await.map_err(|e| {
+            let err = v4::Error::ConnectionFailed(format!("{e:?}"));
+            traces::mark_as_error(&err, Some(Blame::Guest));
+            err
+        })?;
 
         accessor.with(|mut access| {
             let host = access.get();
             host.connections
                 .push(client)
-                .map_err(|_| v4::Error::ConnectionFailed("too many connections".into()))
+                .map_err(|_| {
+                    let err = v4::Error::ConnectionFailed("too many connections".into());
+                    traces::mark_as_error(&err, Some(Blame::Guest));
+                    err
+                })
                 .map(Resource::new_own)
         })
     }
@@ -351,7 +404,9 @@ impl<CF: ClientFactory> spin_world::spin::postgres4_2_0::postgres::HostConnectio
 
         spin_factor_outbound_networking::record_address_fields(&address);
 
-        Self::ensure_address_allowed_async(accessor, &address).await?;
+        Self::ensure_address_allowed_async(accessor, &address)
+            .await
+            .map_err(track_address_check_error_v4)?;
         Self::open_connection_async(accessor, &address, root_ca).await
     }
 }
@@ -396,8 +451,13 @@ impl<CF: ClientFactory> v2::HostConnection for InstanceState<CF> {
         self.otel.reparent_tracing_span();
         spin_factor_outbound_networking::record_address_fields(&address);
 
-        self.ensure_address_allowed(&address).await?;
-        Ok(self.open_connection(&address, None).await?)
+        self.ensure_address_allowed(&address)
+            .await
+            .map_err(v2::Error::from)
+            .map_err(track_address_check_error_v2)?;
+        self.open_connection(&address, None)
+            .await
+            .map_err(v2::Error::from)
     }
 
     #[instrument(name = "spin_outbound_pg.execute", skip(self, connection, params), err(level = Level::INFO), fields(otel.kind = "client", db.system = "postgresql", otel.name = statement))]
@@ -408,11 +468,16 @@ impl<CF: ClientFactory> v2::HostConnection for InstanceState<CF> {
         params: Vec<v2_types::ParameterValue>,
     ) -> Result<u64, v2::Error> {
         self.otel.reparent_tracing_span();
-        Ok(self
-            .get_client(connection)
-            .await?
-            .execute(statement, v2_params_to_v3(params)?)
-            .await?)
+        let params = v2_params_to_v3(params).inspect_err(|e| {
+            traces::mark_as_error(e, Some(Blame::Guest));
+        })?;
+        self.get_client(connection)
+            .await
+            .map_err(v2::Error::from)?
+            .execute(statement, params)
+            .await
+            .map_err(v2::Error::from)
+            .map_err(track_db_error_on_span_v2)
     }
 
     #[instrument(name = "spin_outbound_pg.query", skip(self, connection, params), err(level = Level::INFO), fields(otel.kind = "client", db.system = "postgresql", otel.name = statement))]
@@ -423,11 +488,17 @@ impl<CF: ClientFactory> v2::HostConnection for InstanceState<CF> {
         params: Vec<v2_types::ParameterValue>,
     ) -> Result<v2_types::RowSet, v2::Error> {
         self.otel.reparent_tracing_span();
+        let params = v2_params_to_v3(params).inspect_err(|e| {
+            traces::mark_as_error(e, Some(Blame::Guest));
+        })?;
         Ok(self
             .get_client(connection)
-            .await?
-            .query(statement, v2_params_to_v3(params)?, MAX_HOST_BUFFERED_BYTES)
-            .await?
+            .await
+            .map_err(v2::Error::from)?
+            .query(statement, params, MAX_HOST_BUFFERED_BYTES)
+            .await
+            .map_err(v2::Error::from)
+            .map_err(track_db_error_on_span_v2)?
             .into())
     }
 
@@ -478,4 +549,78 @@ impl<CF: ClientFactory> v1::Host for InstanceState<CF> {
     fn convert_pg_error(&mut self, error: v1::PgError) -> Result<v1::PgError> {
         Ok(error)
     }
+}
+
+/// Mark errors from `ensure_address_allowed` on the current span.
+///
+/// Address check errors where the check infrastructure itself fails (`Other`) are Host-blamed.
+/// All other errors (address not permitted, malformed address, unsupported socket type) are
+/// Guest-blamed since the guest supplied the address.
+fn track_address_check_error_v4(err: v4::Error) -> v4::Error {
+    let blame = match &err {
+        v4::Error::Other(_) => Blame::Host,
+        _ => Blame::Guest,
+    };
+    traces::mark_as_error(&err, Some(blame));
+    err
+}
+
+fn track_address_check_error_v3(err: v3::Error) -> v3::Error {
+    let blame = match &err {
+        v3::Error::Other(_) => Blame::Host,
+        _ => Blame::Guest,
+    };
+    traces::mark_as_error(&err, Some(blame));
+    err
+}
+
+fn track_address_check_error_v2(err: v2::Error) -> v2::Error {
+    let blame = match &err {
+        v2::Error::Other(_) => Blame::Host,
+        _ => Blame::Guest,
+    };
+    traces::mark_as_error(&err, Some(blame));
+    err
+}
+
+/// Mark errors from actual DB client calls (execute/query) on the current span.
+fn track_db_error_on_span_v4(err: v4::Error) -> v4::Error {
+    let blame = match &err {
+        // The guest brings their own database, so connection failures during
+        // execution (dropped connection, auth rejected mid-session, etc.) are
+        // the guest's problem, not the host's.
+        v4::Error::ConnectionFailed(_) => Blame::Guest,
+        v4::Error::BadParameter(_) => Blame::Guest,
+        v4::Error::QueryFailed(_) => Blame::Guest,
+        // The host is responsible for mapping DB wire types to WIT types;
+        // a conversion failure is a host-side limitation or bug.
+        v4::Error::ValueConversionFailed(_) => Blame::Host,
+        v4::Error::Other(_) => Blame::Host,
+    };
+    traces::mark_as_error(&err, Some(blame));
+    err
+}
+
+fn track_db_error_on_span_v3(err: v3::Error) -> v3::Error {
+    let blame = match &err {
+        v3::Error::ConnectionFailed(_) => Blame::Guest,
+        v3::Error::BadParameter(_) => Blame::Guest,
+        v3::Error::QueryFailed(_) => Blame::Guest,
+        v3::Error::ValueConversionFailed(_) => Blame::Host,
+        v3::Error::Other(_) => Blame::Host,
+    };
+    traces::mark_as_error(&err, Some(blame));
+    err
+}
+
+fn track_db_error_on_span_v2(err: v2::Error) -> v2::Error {
+    let blame = match &err {
+        v2::Error::ConnectionFailed(_) => Blame::Guest,
+        v2::Error::BadParameter(_) => Blame::Guest,
+        v2::Error::QueryFailed(_) => Blame::Guest,
+        v2::Error::ValueConversionFailed(_) => Blame::Host,
+        v2::Error::Other(_) => Blame::Host,
+    };
+    traces::mark_as_error(&err, Some(blame));
+    err
 }
