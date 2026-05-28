fc9c62ee31319e7890ddf19510c01ea0b189c078
Ryan Levick <rlevick@akamai.com>
2026-05-28 16:40:33 +0200

    Limit the number of pg/mysql connections an app can have across instances
    Signed-off-by: Ryan Levick <rlevick@akamai.com>

---
+++ crates/factor-outbound-mysql/src/runtime_config/spin.rs
@@ -0,0 +1,29 @@
+use serde::Deserialize;
+use spin_factors::runtime_config::toml::GetTomlValue;
+
+/// Get the runtime configuration for outbound MySQL from a TOML table.
+///
+/// Expects table to be in the format:
+/// ```toml
+/// [outbound_mysql]
+/// max_connections = 10 # optional, defaults to unlimited
+/// ```
+pub fn config_from_table(
+    table: &impl GetTomlValue,
+) -> anyhow::Result<Option<super::RuntimeConfig>> {
+    if let Some(outbound_mysql) = table.get("outbound_mysql") {
+        let toml = outbound_mysql.clone().try_into::<OutboundMysqlToml>()?;
+        Ok(Some(super::RuntimeConfig {
+            max_connections: toml.max_connections,
+        }))
+    } else {
+        Ok(None)
+    }
+}
+
+#[derive(Debug, Default, Deserialize)]
+#[serde(deny_unknown_fields)]
+struct OutboundMysqlToml {
+    #[serde(default)]
+    max_connections: Option<usize>,
+}
