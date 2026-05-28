d988e1cc4138db26ac72305c3323ca6a36b7786a
Ryan Levick <rlevick@akamai.com>
2026-05-28 15:23:49 +0200

    Limit the number of redis connections an app can have across instances
    Signed-off-by: Ryan Levick <rlevick@akamai.com>

---
+++ crates/factor-outbound-redis/src/runtime_config/spin.rs
@@ -0,0 +1,29 @@
+use serde::Deserialize;
+use spin_factors::runtime_config::toml::GetTomlValue;
+
+/// Get the runtime configuration for outbound Redis from a TOML table.
+///
+/// Expects table to be in the format:
+/// ```toml
+/// [outbound_redis]
+/// max_connections = 10 # optional, defaults to unlimited
+/// ```
+pub fn config_from_table(
+    table: &impl GetTomlValue,
+) -> anyhow::Result<Option<super::RuntimeConfig>> {
+    if let Some(outbound_redis) = table.get("outbound_redis") {
+        let toml = outbound_redis.clone().try_into::<OutboundRedisToml>()?;
+        Ok(Some(super::RuntimeConfig {
+            max_connections: toml.max_connections,
+        }))
+    } else {
+        Ok(None)
+    }
+}
+
+#[derive(Debug, Default, Deserialize)]
+#[serde(deny_unknown_fields)]
+struct OutboundRedisToml {
+    #[serde(default)]
+    max_connections: Option<usize>,
+}
