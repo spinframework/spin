2ba948f241e6ea8a8c54b952a6238cdad39eaa27
Brian Hardock <brian.hardock@fermyon.com>
2026-06-02 13:31:41 -0600

    Allow router selection in rust template
    Signed-off-by: Brian Hardock <brian.hardock@fermyon.com>

--- templates/http-rust/content/src/lib.rs
+++ templates/http-rust/content/src/lib.rs
@@ -1,3 +1,60 @@
+{%- case http-router -%}
+{%- when "spin" -%}
+// For the built-in router documentation refer to
+// https://spinframework.dev/rust-components#the-spin-http-router
+use spin_sdk::http::router::{Params, Router};
+use spin_sdk::http::{IntoResponse, Request, StatusCode};
+use spin_sdk::http_service;
+
+/// A simple Spin HTTP component using the built-in router.
+#[http_service]
+async fn handle_{{project-name | snake_case}}(req: Request) -> impl IntoResponse {
+    let mut router = Router::new();
+    router.get("/", index);
+    router.get("/hello/:name", hello);
+    router.any("/*", not_found);
+    router.handle(req).await
+}
+
+async fn index(_req: Request, _params: Params) -> impl IntoResponse {
+    (StatusCode::OK, "Hello, Spin!")
+}
+
+async fn hello(_req: Request, params: Params) -> impl IntoResponse {
+    let name = params.get("name").unwrap_or("world").to_owned();
+    (StatusCode::OK, format!("Hello, {name}!"))
+}
+
+async fn not_found(_req: Request, _params: Params) -> impl IntoResponse {
+    (StatusCode::NOT_FOUND, "Not Found")
+}
+{%- when "axum" -%}
+// For Axum documentation refer to https://docs.rs/axum/latest/axum/
+use axum::extract::Path;
+use axum::routing::get;
+use axum::Router;
+use spin_sdk::http::{IntoResponse, Request};
+use spin_sdk::http_service;
+use tower_service::Service;
+
+/// A simple Spin HTTP component using the Axum router.
+#[http_service]
+async fn handle_{{project-name | snake_case}}(req: Request) -> impl IntoResponse {
+    Router::new()
+        .route("/", get(index))
+        .route("/hello/{name}", get(hello))
+        .call(req)
+        .await
+}
+
+async fn index() -> &'static str {
+    "Hello, Spin!"
+}
+
+async fn hello(Path(name): Path<String>) -> String {
+    format!("Hello, {name}!")
+}
+{%- else -%}
 use spin_sdk::http::{IntoResponse, Request, Response};
 use spin_sdk::http_service;
 
@@ -10,3 +67,4 @@ async fn handle_{{project-name | snake_case}}(req: Request) -> anyhow::Result<im
         .header("content-type", "text/plain")
         .body("Hello World!".to_string()))
 }
+{%- endcase %}
