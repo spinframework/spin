dccead40d7c5c191b055e648f77253236e9fef05
itowlson <ivan.towlson@fermyon.com>
2026-08-10 12:01:31 +1200

    Use async tar crate for OCI
    Signed-off-by: itowlson <ivan.towlson@fermyon.com>

--- crates/oci/src/utils.rs
+++ crates/oci/src/utils.rs
@@ -1,45 +1,55 @@
 //! Utilities related to distributing Spin apps via OCI registries
 
 use anyhow::{Context, Result};
-use flate2::read::GzDecoder;
-use flate2::write::GzEncoder;
+use async_compression::tokio::bufread::GzipDecoder;
+use async_compression::tokio::write::GzipEncoder;
 use spin_common::ui::quoted_path;
 use std::path::{Path, PathBuf};
-use tar::Archive;
+use tokio_tar::Archive;
 
 /// Create a compressed archive of source, returning its path in working_dir
 pub async fn archive(source: &Path, working_dir: &Path) -> Result<PathBuf> {
     let source = source.to_owned();
     let working_dir = working_dir.to_owned();
 
-    tokio::task::spawn_blocking(move || {
-        // Create tar archive file
-        let tar_gz_path = working_dir
-            .join(source.file_name().unwrap())
-            .with_extension("tar.gz");
-        let tar_gz = std::fs::File::create(tar_gz_path.as_path()).context(format!(
+    // Create tar archive file
+    let tar_gz_path = working_dir
+        .join(source.file_name().unwrap())
+        .with_extension("tar.gz");
+    let tar_gz = tokio::fs::File::create(tar_gz_path.as_path())
+        .await
+        .context(format!(
             "Unable to create tar archive for source {}",
             quoted_path(&source)
         ))?;
 
-        // Create encoder
-        // TODO: use zstd? May be more performant
-        let tar_gz_enc = GzEncoder::new(tar_gz, flate2::Compression::default());
+    // Create encoder
+    // TODO: use zstd? May be more performant
+    let tar_gz_enc = GzipEncoder::new(tar_gz);
 
-        // Build tar archive
-        let mut tar_builder = tar::Builder::new(tar_gz_enc);
-        tar_builder.append_dir_all(".", &source).context(format!(
+    // Build tar archive
+    let mut tar_builder = tokio_tar::Builder::new(tar_gz_enc);
+    tar_builder
+        .append_dir_all(".", &source)
+        .await
+        .context(format!(
             "Unable to create tar archive for source {}",
             quoted_path(&source)
         ))?;
 
-        // Finish writing the archive and shut down the encoder.
-        let inner_enc = tar_builder.into_inner()?;
-        inner_enc.finish()?;
+    // Finish writing the archive
+    tar_builder.finish().await?;
 
-        Ok(tar_gz_path)
-    })
-    .await?
+    // Shut down the encoder
+    use tokio::io::AsyncWriteExt;
+    tar_builder
+        .into_inner()
+        .await?
+        .into_inner()
+        .shutdown()
+        .await?;
+
+    Ok(tar_gz_path)
 }
 
 /// Unpack a compressed archive existing at source into dest
@@ -47,13 +57,12 @@ pub async fn unarchive(source: &Path, dest: &Path) -> Result<()> {
     let source = source.to_owned();
     let dest = dest.to_owned();
 
-    tokio::task::spawn_blocking(move || {
-        let decoder = GzDecoder::new(std::fs::File::open(&source)?);
-        let mut archive = Archive::new(decoder);
-        if let Err(e) = archive.unpack(&dest) {
-            return Err(e.into());
-        };
-        Ok(())
-    })
-    .await?
+    let decoder = GzipDecoder::new(tokio::io::BufReader::new(
+        tokio::fs::File::open(&source).await?,
+    ));
+    let mut archive = Archive::new(decoder);
+    if let Err(e) = archive.unpack(&dest).await {
+        return Err(e.into());
+    };
+    Ok(())
 }
